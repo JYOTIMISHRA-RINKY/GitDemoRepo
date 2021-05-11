8584b92f2968286e0d4e8395da7100e65d844881
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2021-05-11 22:02:39 +0530

    my first commit

---
+++ src/main/java/com/ait/demo/GitApp.java
@@ -0,0 +1,17 @@
+package com.ait.demo;
+
+public class GitApp {
+	
+	public void addData(String data) {
+		System.out.println("Data:"+data);
+		if(data.equals("Java Techie")) {
+			System.out.println("Welcome");
+		}
+	}
+
+	public static void main(String[] args) {
+		 System.out.println("Heloo World!");
+
+	}
+
+}
