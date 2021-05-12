625dc02853789e1963513f02f8682a3f17a165a2
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2021-05-12 07:42:38 +0530

    added the getMsg() method

--- src/main/java/com/ait/demo/GitApp.java
+++ src/main/java/com/ait/demo/GitApp.java
@@ -8,12 +8,18 @@ public class GitApp {
 			System.out.println("Welcome");
 		}
 	}
+	
+	public String getMsg() {
+		return "Heloo World";
+	}
 
 	public static void main(String[] args) {
 		 System.out.println("Heloo World!");
 		 GitApp a=new GitApp();
 		 a.addData("Good Morning");
+		 System.out.println( a.getMsg());
 
+		
 	}
 
 }
